5b368dd6256bc489faa0e38ed5956e5e9cf038e0
ShadowDragon2040 <vardaib@kkszki.hu>
2022-11-22 08:55:52 +0100

    javascript kezdet

---
+++ fojava.js
@@ -0,0 +1,29 @@
+document.getElementById("#vadaszgephtml").onclick() = function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#elfogovadaszhtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#bombazohtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#teherhajohtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#utasszalitohtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#csatahajohtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+document.getElementById("#cirkalohtml").onclick()= function(){
+    document.querySelector("#hali").hidden = true;
+}
+
+.onmouseover()
