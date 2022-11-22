5c3fc42d0199be835340bff4e2e6e474a6194b99
ShadowDragon2040 <vardaib@kkszki.hu>
2022-11-22 13:03:41 +0100

    valtozasok

--- fojava.js
+++ fojava.js
@@ -25,5 +25,12 @@ document.getElementById("#csatahajohtml").onclick()= function(){
 document.getElementById("#cirkalohtml").onclick()= function(){
     document.querySelector("#hali").hidden = true;
 }
-
+/*
 .onmouseover()
+let isHidden = false
+isHidden = !isHidden;
+if (isHidden){
+
+}
+.classlist.remove(hidden)
+elsodiv = `dfghjkztrertzuiuhgfcfghuiugftzui` sortöréses altgr+7*/
